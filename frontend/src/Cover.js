a63235727ae2f9bfef19a19a25fe3a27c4db6d85
SY <86911128+everything0216@users.noreply.github.com>
2023-04-25 23:24:06 +0800

    Add files via upload

--- frontend/src/Cover.js
+++ frontend/src/Cover.js
@@ -3,26 +3,24 @@ import React from 'react';
 import logo from './img/IUAlogo.png';
 import { BrowserRouter as Router,Link } from 'react-router-dom';//BrowserRouter
 import { Routes ,Route } from 'react-router-dom';
-import HomePage from './homePage.js';
-import SignIn from './SignIn.js';
 
 const Cover=()=> {
     function Cover() {
       return (
         <div className="Cover"> 
-            <div className="bg">
-                <div className='signIn'>
-                    <div className="title">
-                        <div className="title-text">
+            <div className="Cover_bg">
+                <div className='Cover_signIn'>
+                    <div className="Cover_title">
+                        <div className="Cover_title-text">
                             <h1>IUA</h1>
                         </div>
-                        <div className="title-img">
+                        <div className="Cover_title-img">
                             <img src={logo} alt="IUA" />
                         </div>
                     </div>
                     <br/>
                     <Link to='/SignIn'>
-                        <button className="welcomeBtn"><span>Welcome</span></button>
+                        <button className="Cover_welcomeBtn"><span>Welcome</span></button>
                     </Link>
                 
                 </div>
