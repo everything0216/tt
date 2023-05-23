fb5d03895abc1112b01be2149c25b5e413fd10dc
tiiinyiii <95227577+tiiinyiii@users.noreply.github.com>
2023-05-24 00:26:37 +0800

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import React from 'react';
+import logo from './img/IUAlogo.png';
+
 import {BrowserRouter} from "react-router-dom"//BrowserRouter
 import { Routes ,Route } from 'react-router-dom';
 import Cover from './Cover.js';
@@ -7,18 +9,30 @@ import SignIn from './SignIn.js';
 import Remain from './remain.js';
 import NickName from './nickName';
 import TimeTable from './timeTable';
-
+import HomePage from './homePage.js';
+import Credit from './Credit.js';
+import Core from './Core.js';
+import Must from './Must.js';
+import Graduation from './Graduation.js';
+import Search from './Search';
 
 const App=()=> {
    
     return (
+
            
                 <Routes>
                     <Route path="/" element={<Cover/>} />
-                    <Route path="/signIn" element={<SignIn/>} />
+                    <Route path="/SignIn" element={<SignIn/>} />
                     <Route path="/remain" element={<Remain/>} />
                     <Route path="/nickName" element={<NickName/>} />
                     <Route path="/timeTable" element={<TimeTable/>} />
+                    <Route path="/HomePage" element={<HomePage/>} />
+                    <Route path="/Credit" element={<Credit />}/>
+                    <Route path="/Core" element={<Core />}/>
+                    <Route path="/Must" element={<Must />}/>
+                    <Route path="/Graduation" element={<Graduation />}/>
+                    <Route path="/Search" element={<Search />}/>
                     <Route path="/navbar" element={<navbar/> } />
                 </Routes>
              
--- frontend/src/Cover.js
+++ frontend/src/Cover.js
@@ -3,40 +3,61 @@ import React from 'react';
 import logo from './img/IUAlogo.png';
 import { BrowserRouter as Router,Link } from 'react-router-dom';//BrowserRouter
 import { Routes ,Route } from 'react-router-dom';
+import SignIn from './SignIn.js';
+import Remain from './remain.js';
+import NickName from './nickName';
+import TimeTable from './timeTable';
+import HomePage from './homePage.js';
+import Credit from './Credit.js';
+import Core from './Core.js';
+import Must from './Must.js';
+import Graduation from './Graduation.js';
+import Search from './Search';
 
 const Cover=()=> {
     function Cover() {
-      return (
-        <div className="Cover"> 
-            <div className="Cover_bg">
-                <div className='Cover_signIn'>
-                    <div className="Cover_title">
-                        <div className="Cover_title-text">
-                            <h1>IUA</h1>
+        return (
+            <div className="Cover"> 
+                <div className="Cover_bg">
+                    <div className='Cover_signIn'>
+                        <div className="Cover_title">
+                            <div className="Cover_title-text">
+                                <h1>IUA</h1>
+                            </div>
+                            <div className="Cover_title-img">
+                                <img src={logo} alt="IUA" />
+                            </div>
                         </div>
-                        <div className="Cover_title-img">
-                            <img src={logo} alt="IUA" />
-                        </div>
+                        <br/>
+                        <Link to='/SignIn'>
+                            <button className="Cover_welcomeBtn"><span>Welcome</span></button>
+                        </Link>
+                    
                     </div>
-                    <br/>
-                    <Link to='/SignIn'>
-                        <button className="Cover_welcomeBtn"><span>Welcome</span></button>
-                    </Link>
-                
                 </div>
             </div>
-        </div>
-      );
-    }
+          );
+        }
 
-    return (
+        return (
 
            
-                <Routes>
-                    <Route path="/" element={<Cover />} />
+            <Routes>
+                    <Route path="/" element={<Cover/>} />
+                    <Route path="/SignIn" element={<SignIn/>} />
+                    <Route path="/remain" element={<Remain/>} />
+                    <Route path="/nickName" element={<NickName/>} />
+                    <Route path="/timeTable" element={<TimeTable/>} />
+                    <Route path="/HomePage" element={<HomePage/>} />
+                    <Route path="/Credit" element={<Credit />}/>
+                    <Route path="/Core" element={<Core />}/>
+                    <Route path="/Must" element={<Must />}/>
+                    <Route path="/Graduation" element={<Graduation />}/>
+                    <Route path="/Search" element={<Search />}/>
+                    <Route path="/navbar" element={<navbar/> } />
                 </Routes>
-             
-    );
+         
+        );
 }
 
 export default Cover;
